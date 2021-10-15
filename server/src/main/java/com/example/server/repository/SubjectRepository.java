a7bce9c6f37d71a111ad0d84d71c46e632c05aaa
arnani2212 <arnanplanco@gmail.com>
2021-10-15 14:40:07 +0800

    updated the backend, resolved conflicts and fixed bugs.

--- server/src/main/java/com/example/server/repository/SubjectRepository.java
+++ server/src/main/java/com/example/server/repository/SubjectRepository.java
@@ -1,6 +1,5 @@
 package com.example.server.repository;
 
-import com.example.server.model.Student;
 import com.example.server.model.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
